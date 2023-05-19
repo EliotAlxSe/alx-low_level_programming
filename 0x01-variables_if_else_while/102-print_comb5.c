936fd0210eeacbd00c17389bed8299d77aea38bc
Eliot Malefofane <eliotmalefofanebsc@gmail.com>
2023-05-19 10:39:27 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Prints all possible different combinations of 2 2-digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int num, num2;
+
+	for (num = 0; num <= 98; num++)
+	{
+		for (num2 = num + 1; num2 <= 99; num2++)
+		{
+			putchar((num / 10) + '0');
+			putchar((num % 10) + '0');
+			putchar(' ');
+			putchar((num2 / 10) + '0');
+			putchar((num2 % 10) + '0');
+			if (num == 98 && num2 == 99)
+				continue;
+			putchar(',');
+			putchar(' ');
+
+			
+		}
+	}
+	putchar('\n');
+	return (0);
+}
