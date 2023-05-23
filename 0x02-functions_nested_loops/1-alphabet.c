65bce75849bfd6d4b7e94550cf289d0391afe6b2
Eliot Malefofane <eliotmalefofanebsc@gmail.com>
2023-05-23 09:13:01 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -3,10 +3,11 @@
  * main - prints the alphabet, in lowercase
  * Return : Always 0
  */
-int main(void)
+void print_alphabet(void)
 {
 	char c;
 	for (c = 'a'; c <= 'z'; c++)
 		_putchar(c);
+	_putchar('\n');
 	return (0);
 }
