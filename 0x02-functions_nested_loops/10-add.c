7dc0c1f003c5372e9b9f4b863a15b41a1f3725c7
Eliot Malefofane <eliotmalefofanebsc@gmail.com>
2023-05-24 10:46:08 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - Adds two Integers
+ * @a: first number to be added
+ * @b: second number to be added
+ * Return: The sum of two numbers
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
