b9aaab121085f0c9c983dfab40efebb23024826c
Eliot Malefofane <eliotmalefofanebsc@gmail.com>
2023-05-24 19:38:16 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -5,33 +5,16 @@
  */
 void print_to_98(int n)
 {
-	if(n >= 98)
+	if (n >= 98)
 	{
-		for (; n <= 98; n++)
-		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d", n);
-				for (; n >= 98; n--)
-				{
-					if (n == 98)
-					{
-						printf("%d", n);
-						printf("\n");
-						break;
-					}
-					else
-					{
-						printf("%d", n);
-					}
-				}
-			}
-		}
+		while (n > 98)
+			print("%d", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n <98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
 	}
 }
