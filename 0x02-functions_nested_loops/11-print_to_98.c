56750d82e9a453b17e76af0804b1d132d344fc20
Eliot Malefofane <eliotmalefofanebsc@gmail.com>
2023-05-24 19:39:58 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include "main.h"
 /**
  * print_to_98 - Prints all natural numbers from n to 98
@@ -8,7 +9,7 @@ void print_to_98(int n)
 	if (n >= 98)
 	{
 		while (n > 98)
-			print("%d", n--);
+			printf("%d", n--);
 		printf("%d\n", n);
 	}
 	else
