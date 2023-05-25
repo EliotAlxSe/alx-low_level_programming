583e6bf29a452e5cafe7e7a129972919aa65f6d1
Eliot Malefofane <eliotmalefofanebsc@gmail.com>
2023-05-24 19:29:58 -0700

    11. 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,37 @@
+#include "main.h"
+/**
+ * print_to_98 - Prints all natural numbers from n to 98
+ * @n: A number to be tested
+ */
+void print_to_98(int n)
+{
+	if(n <= 98)
+	{
+		for (; n <= 98; n++)
+		{
+			if (n == 98)
+			{
+				printf("%d", n);
+				printf("\n");
+				break;
+			}
+			else
+			{
+				printf("%d", n);
+				for (; n >= 98; n--)
+				{
+					if (n == 98)
+					{
+						printf("%d", n);
+						printf("\n");
+						break;
+					}
+					else
+					{
+						printf("%d", n);
+					}
+				}
+			}
+		}
+	}
+}
