ba280b31a7cebeecb84b8723edcaa4b237756ffe
Eliot Malefofane <eliotmalefofanebsc@gmail.com>
2023-05-29 13:48:41 -0700

    3. I do not fear computers. I fear the lack of them

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: A string to be printed
+ * Return: void
+ */
+void _puts(char *str)
+{
+
+	if(*str == '\0')
+	{
+		return;
+	}
+    
+    printf("%c", *str);
+    printString(++str);
+}
