f383632d61ae9653c550614baafd9c0350471355
Eliot Malefofane <eliotmalefofanebsc@gmail.com>
2023-06-02 10:56:49 -0700

    0. strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strcat - a pointer function that concatenates two strings
+ * @dest: String to be concatinated
+ * @src: Second string
+ *
+ * Return: A concatinated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int = -1, i;
+	for (i = 0; dest[i] != '\0'; i++)
+	do 
+	{
+		a++;
+		dest[i] = src[a];
+		i++;
+	}while (src[a] != '\0');
+
+	return (dest);
+}
