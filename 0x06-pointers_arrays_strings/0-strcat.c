c9fd70159d764469a4181baceb0d3df9e3b0e141
Eliot Malefofane <eliotmalefofanebsc@gmail.com>
2023-06-02 11:27:28 -0700

    0. strcat

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,12 @@
 char *_strcat(char *dest, char *src)
 {
 	int = -1, i;
+
 	for (i = 0; dest[i] != '\0'; i++)
-		do 
-		{
+		do {
 			a++;
 			dest[i] = src[a];
 			i++;
-		}while (src[a] != '\0');
-
+		  } while (src[a] != '\0');
 	return (dest);
 }
