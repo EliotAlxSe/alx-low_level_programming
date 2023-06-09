239c702299a601da907ddae8c29501c60f00d6e6
Eliot Malefofane <eliotmalefofanebsc@gmail.com>
2023-06-09 09:46:25 -0700

    library

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - function that returns the length of a string.
+ * @s: charecter poiner(A string to be converted to a length)
+ * Return: The length of a pointer @c
+ */
+int _strlen(char *s)
+{
+return (strlen(s));
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ * @n: The value to be tested
+ * Return: Absolute or zero
+ */
+int _abs(int n)
+{
+	return (n > 0 ? n : -n);
+}
