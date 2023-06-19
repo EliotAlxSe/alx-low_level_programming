5e95d88302c29d5af68d163726966675a03418f0
Eliot Malefofane <eliotmalefofanebsc@gmail.com>
2023-06-19 11:11:54 -0700

    Free memory

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - Deletes memory allocated for a struc dog
+ * @d: free struct dog
+ */
+void free_dog(dog_t *d)
+{
+	if(d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
+}
