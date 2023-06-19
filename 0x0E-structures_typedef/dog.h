86d013ea58ba4f12d73fa7243b28ae6e7b7abd19
Eliot Malefofane <eliotmalefofanebsc@gmail.com>
2023-06-19 09:49:17 -0700

    Dog hearder

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - Dog's Information
+ * @name: Name of a dog
+ * @age: Dog's age
+ * @owner: The owner of the dog
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
