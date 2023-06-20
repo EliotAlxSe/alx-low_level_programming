9dfb8ca948ac880fbcf2f071920fe8d53178ed92
Eliot Malefofane <eliotmalefofanebsc@gmail.com>
2023-06-19 18:19:14 -0700

    New dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -61,7 +61,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (dog == NULL)
 		return (NULL);
 	dog->name = malloc(sizeof(char) * (len1 + 1));
-	if (dog->ame == NULL)
+	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
