ac344514adeabdf5a90585d4a747becc05123e66
Eliot Malefofane <eliotmalefofanebsc@gmail.com>
2023-06-19 18:17:04 -0700

    New dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -10,8 +10,8 @@
 int _strlen(char *s)
 {
 	int i;
-	i = 0;
 
+	i = 0;
 	while (s[i] != '\0')
 	{
 		i++;
@@ -28,8 +28,8 @@ int _strlen(char *s)
 char *_strcpy(char *dest, char *src)
 {
 	int len;
-	len = 0;
 
+	len = 0;
 	while (src[len] != '\0')
 	{
 		len++;
@@ -53,6 +53,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 	int len1, len2;
+
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
 	dog = malloc(sizeof(dog_t));
